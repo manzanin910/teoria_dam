70ac71fc62c450d8b4bea2d59d366d2b1d05fbbf
manzanin <147528070+manzanin910@users.noreply.github.com>
2023-10-30 11:55:03 +0100

    Add files via upload

--- galaxy_collision.java
+++ galaxy_collision.java
@@ -17,16 +17,8 @@ public class galaxy_collision{
         InitializeGrid(grid);
 
         grid[1][2][3] = '6';
-        grid[4][5][3] = '2';
-        grid[2][3][3] = '4';
-        grid[2][2][3] = '7';
-        grid[6][4][3] = '3';
-        grid[8][2][3] = '4';
-        
-        printGrid(grid);
-
-        System.out.println(whereTheBigNumber(grid, 2));
-
+        grid[7][5][3] = '2';
+       
         /* 
         // generador de galaxias en posiciones y pesos aleatorios;
         while(value < NumberOfGalax){
@@ -40,14 +32,24 @@ public class galaxy_collision{
             grid[randomXPos][randomYPos][randomZPos] = String.valueOf(randomGalaxNumber).charAt(0);
 
             value++;
-        }
+        }*/
 
         for(int gen = 1; gen <= numberOfGen; gen++){
             System.out.println("<------------------<{Generación " + gen + "}>------------>");
             printGrid(grid);
+
+            for(int i = 0; i < rows; i++){
+                for(int j = 0; j < cols; j++){
+                    for(int k = 0; k < depth; k++){
+                        int lowestDistance = LargeAndCloseNumberFinder(grid, i, j, k);
+                        Displacement(i, j, k, grid, lowestDistance);
+                    }
+                }
+            }
+
             Thread.sleep(2000);
         }
-        */
+        
     }
 
     // inicializar el tablero con espacios vacíos;
@@ -83,22 +85,70 @@ public class galaxy_collision{
         }
     }
 
-    public static String whereTheBigNumber(char grid[][][], int number){
+    // encuentra el número más cercano y grande con un target de referencia;
+    public static int LargeAndCloseNumberFinder(char grid[][][], int x, int y, int z){
 
-        String numeros = "";
-
+        int distanciaMasCercana = 0;;
+        int maxDistance = 80; // cualquier número grande para comparar después;
 
         for(int i = 0; i < grid.length; i++){
             for(int j = 0; j < grid[0].length; j++){
                 for(int k = 0; k < grid[0][0].length; k++){
 
-                    if(Character.getNumericValue(grid[i][j][k]) > number && grid[i][j][k] != '.'){
-                        numeros = "x: " + i + " y: " + j + " z: " + k;
+                    if(grid[i][j][k] != '.' && Integer.valueOf(grid[i][j][k]) > grid[x][y][z]){
+
+                        // hay que calcular la diferencia en coordenadas, tienes que separar y restar cada una:
+                        int Distance = Math.abs(x - i) + Math.abs(y - j) + Math.abs(z - k);
+                    
+                        if(maxDistance > Distance){
+
+                            maxDistance = Distance;
+                            distanciaMasCercana = maxDistance;
+                        }
                     }
+                    
                 }
             }
         }
 
-        return numeros;
+        return distanciaMasCercana;
+    }
+
+    // Desplazamiento de la galaxia hacia otra según la distancia a la que está;
+    public static void Displacement(int x, int y, int z, char[][][] grid, int distance){
+
+        int[] dx = { -1, -1, -1, 0, 0, 1, 1, 1, 0, 0, 0, 0 };
+        int[] dy = { -1, 0, 1, -1, 1, -1, 0, 1, 0, 0, 0, 0 };
+        int[] dz = { 0, 0, 0, 0, 0, 0, 0, 0, -1, 1, 0, 0 };
+
+        int X = x;
+        int Y = y;
+        int Z = z;
+
+        for(int i = 0; i < 12; i++){
+            int newX = x + dx[i];
+            int newY = y + dy[i];
+            int newZ = z + dz[i];
+
+            int newDistance = Math.abs(newX - x) + Math.abs(newY - y) + Math.abs(newZ - z);
+
+            if(newX >= 0 && newX < grid.length && newY >= 0 && newY < grid[0].length && newZ >= 0 && newZ < grid[0][0].length){
+                if(newDistance < distance && grid[newX][newY][newZ] == '.'){
+
+                    distance = newDistance;
+                    
+                    X = newX;
+                    Y = newY;
+                    Z = newZ;
+                }
+            }
+        }
+
+        if (grid[X][Y][Z] == '.'){
+
+            grid[X][Y][Z] = grid[x][y][z];
+            grid[x][y][z] = '.';  // Restablece el espacio original como vacío;
+        }
+        
     }
 }
